a166c7aaca6fd89f5c1735150cb032547fedd7ee
mouboo <peterkvillegard@protonmail.com>
2025-05-31 11:51:17 +0200

    error checking plus bug found

--- internal/webserver/handler.go
+++ internal/webserver/handler.go
@@ -8,6 +8,8 @@ import(
 	"github.com/mouboo/cowsayaas/internal/cowsay"
 )
 
+// TODO: fix bug if width is less than 5 (messes up slice boundaries)
+// Fix could be to let width refer to text width sans borders?
 func PlainHandler(w http.ResponseWriter, r *http.Request) {
 	//Default width
 	width := 44
@@ -15,14 +17,23 @@ func PlainHandler(w http.ResponseWriter, r *http.Request) {
 	// to an int, use that number instead.
 	widthStr := r.URL.Query().Get("width")
 	if widthStr != "" {
-		if w, err := strconv.Atoi(widthStr); err == nil {
-			width = w
+		if widthParsed, err := strconv.Atoi(widthStr); err == nil {
+			if widthParsed > 0 {
+				width = widthParsed
+			} else {
+				http.Error(w, "Width must be a positive number", http.StatusBadRequest)
+				return
+			}
+		} else {
+			http.Error(w, "Invalid width parameter", http.StatusBadRequest)
+			return
 		}
 	}
 	
 	text := r.URL.Query().Get("text")
 	if text == "" {
 		http.Error(w, "Missing text parameter", http.StatusBadRequest)
+		return
 	}
 	response := cowsay.RenderCowsay(text, width)
 	// Write to the ResponseWriter
